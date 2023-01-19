7d586806dbb1d8a10e4fbd1db55b0d62833ddf0c
Ismaelleon <Ismaelleon@protonmail.com>
2023-01-19 03:29:37 -0300

    feat: add authentication pages (signup, login)

---
+++ pages/login.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'next/link';
+
+// Import components
+import HTMLHead from '../components/htmlhead';
+import Header from '../components/header';
+
+const LogIn: React.FunctionComponent = () => {
+	return (
+		<div>
+			<HTMLHead />
+			<Header />
+			<main className="flex justify-center items-center flex-col h-screen">
+				<form className="flex flex-col ">
+					<input type="email" placeholder="E-mail" className="border rounded-md p-3 my-2" />
+					<input type="password" placeholder="Password" className="border rounded-md p-3 my-2" />
+					<input type="submit" value="Log In" className="bg-emerald-500 text-white font-bold py-3 rounded-md text-center my-2" />
+				</form>
+				<Link href="/signup" className="text-sm my-2 text-emerald-500 underline">You don't have an account? Sign Up</Link>
+			</main>
+		</div>
+	);
+};
+
+export default LogIn;
---
+++ pages/signup.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'next/link';
+
+// Import components
+import HTMLHead from '../components/htmlhead';
+import Header from '../components/header';
+
+const SignUp: React.FunctionComponent = () => {
+	return (
+		<div>
+			<HTMLHead />
+			<Header />
+			<main className="flex justify-center items-center flex-col h-screen">
+				<form className="flex flex-col ">
+					<input type="email" placeholder="E-mail" className="border rounded-md p-3 my-2" />
+					<input type="password" placeholder="Password" className="border rounded-md p-3 my-2" />
+					<input type="submit" value="Sign Up" className="bg-emerald-500 text-white font-bold py-3 rounded-md text-center my-2" />
+				</form>
+				<Link href="/login" className="text-sm my-2 text-emerald-500 underline">Already have an account? Log In</Link>
+			</main>
+		</div>
+	);
+};
+
+export default SignUp;
