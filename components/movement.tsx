23880654d191d032755b42e07eef4cf450f645e9
Ismaelleon <Ismaelleon@protonmail.com>
2023-01-19 04:47:11 -0300

    feat: create /balance route

---
+++ components/movement.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { IoMdArrowUp, IoMdArrowDown } from 'react-icons/io';
+
+interface MovementProps {
+	amount: number;
+	date: string;
+	hour: string;
+	profit: boolean;
+}
+
+type Props = MovementProps;
+
+const Movement = ({ amount, date, hour, profit }: Props) => {
+	return (
+		<li className="flex flex-row py-3 border-b">
+			<span>
+				{profit ?
+					<IoMdArrowUp className="text-green-500 text-2xl" />	:
+					<IoMdArrowDown className="text-red-500 text-2xl" />}
+			</span>
+			<div className="flex flex-col">
+				<span className="text-md font-semibold leading-4 mb-0">${amount} USD</span>
+				<span className="text-sm text-slate-600">{date}, {hour}</span>
+			</div>
+		</li>
+	);
+};
+
+export default Movement;
---
+++ pages/balance.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { IoMdEye, IoMdEyeOff } from 'react-icons/io';
+
+// Import components
+import HTMLHead from '../components/htmlhead';
+import Header from '../components/header';
+import Movement from '../components/movement';
+
+const Balance: React.FunctionComponent = () => {
+	const [balanceHidden, setBalanceHidden] = useState(true);
+
+	return (
+		<div>
+			<HTMLHead />	
+			<Header />
+			<main className="p-3 mt-10">
+				<div className="bg-gradient-to-r from-emerald-500 to-emerald-600 p-3 rounded-md my-3">
+					<header className="flex flex-row justify-between items-center">
+						<h2 className="text-xl text-white font-bold">Balance</h2>
+						<button onClick={() => setBalanceHidden(!balanceHidden)}>
+							{balanceHidden ?
+								<IoMdEyeOff className="text-white text-2xl" /> :
+								<IoMdEye className="text-white text-2xl" />}
+						</button>
+					</header>
+					<span className="text-3xl text-white font-bold">$***</span>
+				</div>
+				<div className="border-t my-3 p-3">
+					<span className="text-base text-black font-medium">Movements</span>
+					<ul>
+						<Movement amount={2500} date={'Today'} hour={'3:25PM'} profit={true} />
+						<Movement amount={1300} date={'Today'} hour={'1:45PM'} profit={false} />
+						<Movement amount={3000} date={'Today'} hour={'1:30PM'} profit={true} />
+						<Movement amount={900} date={'Today'} hour={'12:55PM'} profit={true} />
+					</ul>
+				</div>
+			</main>
+		</div>
+	);
+};
+
+export default Balance;
